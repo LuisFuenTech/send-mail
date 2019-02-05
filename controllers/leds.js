c6264883909e480b3366e40bb1d7c558473aef47
Luis Fuentes <luisfuau10@gmail.com>
2019-02-04 22:28:18 -0500

    Names changed

--- controllers/leds.js
+++ controllers/leds.js
@@ -3,8 +3,8 @@ const { leds } = require("../db.json");
 const switchLed = (req, res) => {
   const { id, action } = req.params;
 
-  leds.forEach((led, i) => {
-    if (led.id === Number(id)) {
+  leds.forEach(led => {
+    if (led.name === Number(id)) {
       led.isOn = action == "on" ? true : action == "off" ? false : "";
     }
   });
