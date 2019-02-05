71f052ced8b21a48cacf883bd8de17dcb07ab738
Luis Fuentes <luisfuau10@gmail.com>
2019-02-05 00:09:44 -0500

    Sensor dht11 added

--- routes/clients.js
+++ routes/clients.js
@@ -4,6 +4,7 @@ const ledsController = require("../controllers/leds");
 
 app.get("/led/:id/:action", ledsController.switchLed);
 app.get("/get-leds", ledsController.getLeds);
+app.get("/get-info", ledsController.getInfo);
 app.get("/read-sensor", ledsController.readSensors);
 app.get("/sensors/:temp/:humidity", ledsController.showValues);
 
