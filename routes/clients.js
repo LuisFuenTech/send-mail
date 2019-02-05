66ff67211964999d79c3173902e0d5264f7ad8a3
Luis Fuentes <luisfuau10@gmail.com>
2019-02-04 22:47:11 -0500

    Sensor dht11 added

--- routes/clients.js
+++ routes/clients.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express.Router();
 const ledsController = require("../controllers/leds");
 
-app.post("/led/:id/:action", ledsController.switchLed);
+app.get("/led/:id/:action", ledsController.switchLed);
 app.get("/get-leds", ledsController.readLeds);
 
 module.exports = app;
