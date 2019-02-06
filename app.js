cdb20b5c5aef6c89ba8d2fafc9a90320dd876734
Fuentech <luisfuau10@gmail.com>
2019-02-06 11:49:46 -0500

    Cors added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const { usersRoutes } = require("./users/index");
+
+app.use(cors());
+app.get("/", function(req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
--- users/usersController.js
+++ users/usersController.js
@@ -5,27 +5,21 @@ module.exports.switchLed = (req, res) => {
   const { id, action } = req.params;
 
   axios
-    .get(`http://192.168.0.14/led?led=${id}&action=${action}`)
+    .get(`http://192.168.1.60/led?led=${id}&action=${action}`)
     .then(response => res.status(200).send(response.data))
     .catch(error => res.status(503).send({ Error: "Can't reach led" }));
 };
 
 module.exports.getInfo = (req, res) => {
-  res.status(200).send({
-    message: "Hey"
-  });
+  axios
+    .get("http://192.168.1.60/hello")
+    .then(response => res.status(200).send(response.data))
+    .catch(error => res.status(503).send({ Error: error }));
 };
 
 module.exports.readSensors = (req, res) => {
   axios
-    .get(`http://192.168.0.14/sensor`)
+    .get(`http://192.168.1.60/sensor`)
     .then(response => res.status(200).send(response.data))
     .catch(error => res.status(503).send({ Error: "Can't reach sensor" }));
 };
-/*
-module.exports = {
-  switchLed,
-  getInfo,
-  readSensors
-};
-*/
