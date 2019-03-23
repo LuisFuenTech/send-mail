b6a803387b7cde92e7d460f96436307a565c80ba
Luis Fuentes <luisfuau10@gmail.com>
2019-03-22 20:36:08 -0500

    Fix 2

--- config/database.js
+++ config/database.js
@@ -1,17 +1,14 @@
 const port = process.env.PORT || 3000;
 const dbPort = process.env.DB_PORT || 27017;
-const dbUrl = process.env.MONGODB_URL || "mongodb://localhost:27017/smarthome";
+const dbUrl = process.env.DB_URL || "localhost";
 const dbCollection = process.env.DB_COLLECTION || "smarthome";
 const app = require("../bin/WWW");
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://fuentech:Atlasfuentech2018@mycluster-aeuut.mongodb.net/test?retryWrites=true",
-    {
-      useNewUrlParser: true
-    }
-  )
+  .connect(`mongodb://${dbUrl}:${dbPort}/${dbCollection}`, {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log("Connected to database");
     app.listen(port, () => {
@@ -22,6 +19,4 @@ mongoose
     console.log(`Can't connect to database: ${err}`);
   });
 
-module.exports = {
-  mongoose
-};
+module.exports.mongoose = mongoose;
