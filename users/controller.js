8a6c9d90330b3361b103b0b87c14b98fad3557ea
Luisfuentech <luisfuentes.au@gmail.com>
2019-07-23 00:24:31 -0500

    Promises

--- users/controller.js
+++ users/controller.js
@@ -1,43 +1,70 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = (req, res) => {
-  //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-  // Definimos el transporter
-  var transporter = nodemailer.createTransport({
+const recipients = [
+  "seeacing2018@gmail.com",
+  "nerirevelacion@gmail.com",
+  "luisfau@misena.edu.co",
+  "lfuentesa@unicartagena.edu.co"
+];
+
+const sendEmail = async (req, res) => {
+  let successful = [];
+  let failed = [];
+
+  let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // use SSL
+    secure: false,
     auth: {
-      user: "seeacing2018@gmail.com",
-      pass: "Mail@seeac2019"
+      user: "nerirevelacion@gmail.com",
+      pass: "Sanfelipeneri10"
     },
     tls: {
       rejectUnauthorized: false
     }
   });
-  // Definimos el email
-  var mailOptions = {
-    from: "Prueba 2 Luis Fuentes <seeacing2018@gmail.com>",
-    to: "info@seeacingenieriasas.com, seeacing2018@gmail.com",
-    subject: "Saludos from Nodemailer",
-    text: "Hola guapo",
-    html: `
-    <h3>Hola Guap@s</h3> 
-    <p>Esto es un párrafo de prueba, enviado desde el aplicativo web creado para optimizar
-    el envío de correos masivos</p>
-    <a href="https://github.com/luisfuentech">Proyectos de Luis Fuentes :) </a>`
-  };
-  // Enviamos el email
-  transporter.sendMail(mailOptions, function(err, info) {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ error: err.message });
-    } else {
-      console.log("Email sent");
-      res.status(200).json({ Ok: "Email sent" });
-    }
+
+  await recipients.forEach(async (to, index) => {
+    let mailOptions = {
+      from: "Prueba 2",
+      to: to,
+      subject: "Saludos from Nodemailer",
+      text: "Hola guapo",
+      html: `
+      <h3>Hola Guap@s</h3> 
+      <p>Esto es un párrafo de prueba, enviado desde el aplicativo web creado para optimizar
+      el envío de correos masivos</p>
+      <a href="https://github.com/luisfuentech">Proyectos de Luis Fuentes :) </a>`
+    };
+
+    await send(transporter, mailOptions)
+      .then(() => {
+        console.log("Success");
+        successful.push({ success: true, to });
+      })
+      .catch(err => {
+        console.log("Failed", err);
+        failed.push({ success: false, to, err });
+      });
+
+    console.log(successful);
+    console.log(failed);
   });
+
+  res.status(200).json({ Ok: "Email sent" });
 };
+
+async function send(transporter, mailOptions) {
+  return await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return reject(error);
+      } else {
+        return resolve();
+      }
+    });
+  });
+}
 
 module.exports = {
   sendEmail
