8cc1405931e71b86614f9623332e1e7190d5fd31
luisfuentech <luisfuentes.au@gmail.com>
2019-07-23 16:13:53 -0500

    Send from any service

--- user/controller.js
+++ user/controller.js
@@ -1,11 +1,113 @@
 const nodemailer = require("nodemailer");
 const { Email } = require("../email/index");
 
+const manyRecipients = [
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "McDonald's <McDonalds@news.mcdonalds.com.co>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Coursera <no-reply@m.mail.coursera.org>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Twitter <info@twitter.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "McDonald's <McDonalds@news.mcdonalds.com.co>",
+  "Coursera <no-reply@m.mail.coursera.org>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Espanol.Free-eBooks.net <noticias@espanol.free-ebooks.net>",
+  "Facebook Business <advertise-noreply@support.facebook.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Twitter <info@twitter.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Coursera <no-reply@m.mail.coursera.org>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "McDonald's <McDonalds@news.mcdonalds.com.co>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Coursera <no-reply@m.mail.coursera.org>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "mobifriends <mobifriends@mobifriends.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Coursera <no-reply@m.mail.coursera.org>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Coursera <no-reply@m.mail.coursera.org>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook Business <advertise-noreply@support.facebook.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Luis Alberto Fuentes Auraad <luiefuentes9@hotmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Falabella.com <newsletter@co.falabella.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Falabella.com <newsletter@co.falabella.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Coursera <no-reply@m.mail.coursera.org>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "Facebook <notification@facebookmail.com>",
+  "mobifriends <mobifriends@mobifriends.com>",
+  "Facebook <notification@facebookmail.com>"
+];
+
 const recipients = [
-  "seeacing2018@gmail.com",
   "nerirevelacion@gmail.com",
-  "lfuentesa@unicartagena.edu.co",
-  "luisfau@misesa.edu.co"
+  "seecing2018@gmail.com",
+  "luisfau@misena.edu.co"
 ];
 
 const sendGmail = async (req, res) => {
@@ -19,13 +121,16 @@ const sendGmail = async (req, res) => {
 };
 
 const sendOutlook = async (req, res) => {
-  sender(
-    "outlook",
-    "smtp-mail.outlook.com",
-    "seeac.ingenieriasas@outlook.com",
-    "Outlook@seeac2019",
-    res
-  );
+  for (let i = 0; i < 2; i++) {
+    console.log(`Fase ${i + 1}`);
+    await sender(
+      "outlook",
+      "smtp.office365.com",
+      "seeac.ingenieriasas@outlook.com",
+      "Outlook@seeac2019",
+      res
+    );
+  }
 };
 
 const deleteAll = async (req, res) => {
@@ -33,6 +138,93 @@ const deleteAll = async (req, res) => {
   console.log("Delete");
 
   res.status(200).json({ msg: "Hola guapo" });
+};
+
+const sendAny = async (req, res) => {
+  const { service } = req.params;
+  console.log("TCL: sendAny -> service", service);
+
+  let successful = [];
+  let failed = [];
+
+  let mailOptions = {
+    from: "Mail test",
+    subject: "Greetings from Nodemailer",
+    text: "Hey baby!",
+    html: `
+    <h3>Hola Guap@s</h3> 
+    <p>Esto es un párrafo de prueba, enviado desde el aplicativo web</p>`
+  };
+
+  if (service == "gmail") {
+    var transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      auth: {
+        user: "nerirevelacion@gmail.com",
+        pass: "Sanfelipeneri10"
+      },
+      tls: {
+        rejectUnauthorized: false
+      },
+      secure: false
+    });
+  } else {
+    var transporter = nodemailer.createTransport({
+      host: "smtp.office365.com",
+      port: 587,
+      auth: {
+        user: "seeac.ingenieriasas@outlook.com",
+        pass: "Outlook@seeac2019"
+      },
+      tls: {
+        rejectUnauthorized: false
+      },
+      secureConnection: false
+    });
+  }
+
+  console.log("TCL: sendAny -> transporter", transporter);
+
+  for (const [index, recipient] of recipients.entries()) {
+    mailOptions.to = recipient;
+    console.log("TCL: sendAny -> recipient", recipient);
+
+    const email = new Email();
+    email.from = transporter.options.auth.user;
+    email.to = recipient;
+
+    try {
+      const response = await send(transporter, mailOptions);
+
+      if (response) {
+        console.log("Success");
+        email.sent = true;
+        await email.save();
+        successful.push({ success: true, to: response });
+      } else {
+        console.log("Failed");
+        email.sent = false;
+        await email.save();
+        failed.push({ success: false, to: response });
+      }
+    } catch (error) {
+      console.log("Failed");
+      email.sent = false;
+      await email.save();
+      failed.push({ success: false, to: recipient });
+    }
+  }
+
+  console.log("Success:", successful);
+  console.log("Failure", failed);
+
+  res.status(200).json({
+    service,
+    status: "Emails sent",
+    Success: successful,
+    Failure: failed
+  });
 };
 
 async function sender(service, host, user, pass, res) {
@@ -57,8 +249,7 @@ async function sender(service, host, user, pass, res) {
     text: "Hey baby!",
     html: `
     <h3>Hola Guap@s</h3> 
-    <p>Esto es un párrafo de prueba, enviado desde el aplicativo web creado para optimizar
-    el envío de correos masivos</p>`
+    <p>Esto es un párrafo de prueba, enviado desde el aplicativo web</p>`
   };
 
   service == "gmail"
@@ -73,34 +264,33 @@ async function sender(service, host, user, pass, res) {
     if (mailSent == null) {
       console.log("Eres nuevo");
 
-      const email = new Email();
+      var email = new Email();
       email.from = user;
       email.to = recipient;
 
       try {
         const response = await send(transporter, mailOptions);
 
-        if (response) {
+        if (response.accepted) {
           console.log("Success");
           email.sent = true;
           await email.save();
-          successful.push({ success: true, to: response });
+          successful.push({ success: true, to: response.envelope.to[0] });
         } else {
           console.log("Failed");
           email.sent = false;
           await email.save();
-          failed.push({ success: false, to: response });
+          failed.push({ success: false, to: response.envelope.to[0] });
         }
       } catch (error) {
         email.sent = false;
         await email.save();
-        failed.push({ success: false, to: recipient });
+        failed.push({ success: false, to: recipient.envelope.to[0] });
       }
     } else if (mailSent.sent) {
       console.log("Ya te enviaron", { to: mailSent.to, sent: mailSent.sent });
     } else {
       console.log("Te voy a enviar");
-
       try {
         const response = await send(transporter, mailOptions);
 
@@ -138,18 +328,30 @@ function send(transporter, mailOptions) {
   return new Promise((resolve, reject) => {
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
-        console.log("Error:", error, "To:", info.envelope.to[0]);
-        return reject(info.envelope.to[0]);
+        console.log("Error:", error, "To:", mailOptions.to);
+        return reject(error);
       } else {
-        console.log("Info:", info.envelope.to[0]);
-        return resolve(info.envelope.to[0]);
+        console.log("Info:", info);
+        return resolve(info);
       }
     });
   });
 }
 
+let objectInfo = {
+  accepted: ["luisfau@misena.edu.co"],
+  rejected: [],
+  envelopeTime: 326,
+  messageTime: 519,
+  messageSize: 645,
+  response: "250 2.0.0 OK  1563915956 e128sm14676406vkh.10 - gsmtp",
+  envelope: { from: "", to: ["luisfau@misena.edu.co"] },
+  messageId: "<4cd7762b-7cf2-dbbe-6edb-4582f50a9349@Gilberto>"
+};
+
 module.exports = {
   sendGmail,
   sendOutlook,
-  deleteAll
+  deleteAll,
+  sendAny
 };
--- user/routes.js
+++ user/routes.js
@@ -4,5 +4,6 @@ const { userController } = require("./index");
 app.get("/send-gmail", userController.sendGmail);
 app.get("/send-outlook", userController.sendOutlook);
 app.get("/send-delete", userController.deleteAll);
+app.get("/send-any/:service", userController.sendAny);
 
 module.exports = app;
